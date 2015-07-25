c9fcbb999f56926a18e0f50e64e66e8c7b3ee13d
Mike Ebinum <mebinum@gmail.com>
2015-07-25 16:25:02 +1000

    added bridging header

---
+++ src/iOS/octosniffle-Bridging-Header.h
@@ -0,0 +1,17 @@
+//
+//  octosniffle-Bridging-Header.h
+//  octosniffle
+//
+//  Created by Mike Ebinum on 25/07/2015.
+//  Copyright (c) 2015 Facebook. All rights reserved.
+//
+
+#ifndef octosniffle_octosniffle_Bridging_Header_h
+#define octosniffle_octosniffle_Bridging_Header_h
+
+#import "RCTBridgeModule.h"
+#import "RCTConvert.h"
+#import "RCTBridge.h"
+#import "RCTEventDispatcher.h"
+
+#endif
